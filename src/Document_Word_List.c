0a79afeb148bc6450ea52592b5cf37dc9f8b4a6d
PC178 <N/A>
2022-05-22 22:24:32 +0200

    Add technique to avoid multiple results in one intersect set.

--- src/Document_Word_List.c
+++ src/Document_Word_List.c
@@ -181,9 +181,18 @@ Intersect_Data_With_Document_Word_List
     intersection_result->next_free_array = object->next_free_array;
     intersection_result->intersection_data = true;
 
+    // Array, welches erkennt, ob in der Schnittmenge ein Wert mehrfach hinzugefuegt werden soll
+    const size_t multiple_guard_realloc_step = 1000;
+    size_t size_multiple_guard = multiple_guard_realloc_step;
+
+    _Bool* multiple_guard = (_Bool*) CALLOC(size_multiple_guard, sizeof (_Bool));
+    ASSERT_ALLOC(multiple_guard, "Cannot create the multiple guard !", size_multiple_guard * sizeof (_Bool));
+
     // Fuer den ersten Versuch die naive Variant fuer die Bestimmung der Schnittmenge
     for (size_t i = 0; i < object->number_of_arrays; ++ i)
     {
+        memset(multiple_guard, '\0', size_multiple_guard * sizeof (_Bool));
+
         for (size_t i2 = 0; i2 < object->arrays_lengths [i]; ++ i2)
         {
             // Jedes Element des Arrays mit jedem Element der Testdaten miteinander vergleichen
@@ -191,12 +200,28 @@ Intersect_Data_With_Document_Word_List
             {
                 if (object->data [i][i2] == data [i3])
                 {
-                    intersection_result->data [i][intersection_result->arrays_lengths [i]] = data [i3];
-                    intersection_result->arrays_lengths [i] ++;
+                    // Reicht der Speicher aus ?
+                    if (data [i3] > size_multiple_guard)
+                    {
+                        size_multiple_guard += multiple_guard_realloc_step;
+                        multiple_guard = (_Bool*) REALLOC(multiple_guard, size_multiple_guard * sizeof (_Bool));
+                        ASSERT_ALLOC(multiple_guard, "Cannot increase the memory for the multiple guard !",
+                                size_multiple_guard * sizeof (_Bool));
+                    }
+
+                    if (multiple_guard [data [i3]] == false)
+                    {
+                        intersection_result->data [i][intersection_result->arrays_lengths [i]] = data [i3];
+                        intersection_result->arrays_lengths [i] ++;
+                        multiple_guard [data [i3]] = true;
+                    }
                 }
             }
         }
     }
+
+    printf ("Used size for the multiple guard: %zu\n", size_multiple_guard * sizeof (_Bool));
+    FREE_AND_SET_TO_NULL(multiple_guard);
 
     return intersection_result;
 }
