13ee3f5772117921579e2c7d24562759868934b2
PC178 <N/A>
2022-05-22 21:22:52 +0200

    Using the function to show both - data and attributes.

--- src/Tests/Document_Word_List_Test.c
+++ src/Tests/Document_Word_List_Test.c
@@ -72,7 +72,7 @@ extern _Bool TEST_Intersection (void)
 
     for (size_t i = 0; i < COUNT_ARRAY_ELEMENTS(test_data); ++ i)
     {
-        Show_Data_From_Document_Word_List(test_data [i]);
+        Show_Data_And_Attributes_From_Document_Word_List(test_data [i]);
     }
 
     // Das erste Objekt ist das Original-Objekt, welches die Menge enthalt, worauf die Schnittmengen gebildet werden
@@ -90,7 +90,7 @@ extern _Bool TEST_Intersection (void)
     PUTS_FFLUSH("\n");
     for (size_t i = 0; i < COUNT_ARRAY_ELEMENTS(intersection_objects); ++ i)
     {
-        Show_Data_From_Document_Word_List(intersection_objects [i]);
+        Show_Data_And_Attributes_From_Document_Word_List(intersection_objects [i]);
     }
 
     // Beide Arrays loeschen
