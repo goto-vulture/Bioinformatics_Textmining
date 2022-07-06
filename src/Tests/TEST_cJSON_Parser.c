f1e6d8153089b797a683274d45e87bfc4007ab5a
Gyps <N/A>
2022-07-06 13:38:16 +0200

    Add new translation unit to test the cJSON parser usage.

---
+++ src/Tests/TEST_cJSON_Parser.c
@@ -0,0 +1,10 @@
+/**
+ * @file TEST_cJSON_Parser.c
+ *
+ * @brief In this translation unit are a few tests for the cJSON parser usage.
+ *
+ * @date 06.07.2022
+ * @author am1
+ */
+
+#include "TEST_Document_Word_List.h"
---
+++ src/Tests/TEST_cJSON_Parser.h
@@ -0,0 +1,29 @@
+/**
+ * @file TEST_cJSON_Parser.h
+ *
+ * @brief In this translation unit are a few tests for the cJSON parser usage.
+ *
+ * @date 06.07.2022
+ * @author am1
+ */
+
+
+#ifndef TEST_CJSON_PARSER_H
+#define TEST_CJSON_PARSER_H ///< Include-Guard
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+
+
+// ...
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* TEST_CJSON_PARSER_H */
