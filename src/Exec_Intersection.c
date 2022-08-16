84a65743362cd775aa7f84723c6d03dc4fb09731
C720n <N/A>
2022-08-16 14:22:44 +0200

    Extend the description of the execution steps.

--- src/Exec_Intersection.c
+++ src/Exec_Intersection.c
@@ -39,12 +39,37 @@ Exec_Intersection_Process_Print_Function
  *
  * Execution steps:
  * - Read files and extract the tokens                                      (Two Token_List_Container)
- * - Create a token int mapping list                                        (One Token_Int_Mapping)
- * - Use the token int mapping for the creation of a mapped token container (Two Document_Word_List)
- * - Create the intersections and save the information in the output file
+ *      -- Token_List_Container is a container for token lists
+ *      -- Every token list represents the tokens of one tokens array in the source JSON file
  *
- * There will be NO input value tests, because NaN, +Inf, ... are good possibilities to say the function, that the
- * calculation should not stopped before the calculation is completed.
+ * - Create a token int mapping list                                        (One Token_Int_Mapping)
+ *      -- The token int mapping give every token a unique integer value (This idea was chosen to speed up the
+ *         intersection process, because integer comparisons are significant faster than string comparisons, especially
+ *         with long strings)
+ *      -- Also every token will be unique in the mapping object
+ *      -- To avoid searching in the full data, a pseudo hash function was implemented. There are 100 data buckets
+ *      -- The "hash sum" of a string is the sum of all char mod 100
+ *      -- The integer values have the chosen bucket encoded in it itself
+ *      -- the first two lowest digits (in decimal system) encodes the bucket. E.g.: xxx10 means, that this integer can
+ *         found in the 11. bucket, when it exists in the mapping data
+ *
+ * - Use the token int mapping for the creation of a mapped token container (Two Document_Word_List)
+ *      -- A Document_Word_List contains a 2 dimensional integer array
+ *      -- In this array is the data for the intersection or for the intersection result (Yes Document_Word_List will
+ *         be used for the intersection input data and for the intersection result !)
+ *      -- Every array in the 2 dimensional array represents the data of one tokens array
+ *      -- This means, that the results are focused on the tokens array from the input data
+ *      -- This is necessary, because one of the goals of this project is to find the intersections between the tokens
+ *         arrays in the input data
+ *
+ * - Create the intersections and save the information in the output file
+ *      -- At the end the intersection between two Document_Word_List objects will be calculated
+ *      -- The results will be written in the result file
+ *
+ *
+ *
+ * In this function will be NO input value tests, because NaN, +Inf, ... are good possibilities to say the function,
+ * that the calculation should not stopped before the it is completed.
  *
  * Every invalid value will be interpreted in this way!
  *
@@ -106,6 +131,13 @@ Exec_Intersection
     const size_t length_of_longest_token_container = MAX(Get_Lengh_Of_Longest_Token_Container(token_container_input_1),
             Get_Lengh_Of_Longest_Token_Container(token_container_input_2));
 
+    // token_container_input_1->next_free_element and token_container_input_2->next_free_element
+    // are the number of arrays in the two Token_List_Container. The meaning is, that every array in a
+    // Token_List_Container represents a token array in the input JSON files
+    // And length_of_longest_token_container is the length of the longest array. The idea is, that a Document_Word_List
+    // will be created with the worst case in the memory usage. This is massive inefficient, but for the development it
+    // is okay.
+    // => ! It will be changed for real use cases ! <=
     struct Document_Word_List* source_int_values_1 =
             Create_Document_Word_List(token_container_input_1->next_free_element, length_of_longest_token_container);
     struct Document_Word_List* source_int_values_2 =
